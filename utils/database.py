ec4aac071a2764265f314aa2669b4466974a875d
Leoinferos <brijesh.pandya666@gmail.com>
2026-07-06 12:42:12 +0530

    Fix Supabase session handling

--- utils/database.py
+++ utils/database.py
@@ -1,7 +1,17 @@
 from supabase import create_client
 import streamlit as st
 
-supabase = create_client(
-    st.secrets["SUPABASE_URL"],
-    st.secrets["SUPABASE_KEY"]
-)
+
+def get_supabase():
+
+    if "supabase" not in st.session_state:
+
+        st.session_state.supabase = create_client(
+            st.secrets["SUPABASE_URL"],
+            st.secrets["SUPABASE_KEY"]
+        )
+
+    return st.session_state.supabase
+
+
+supabase = get_supabase()
